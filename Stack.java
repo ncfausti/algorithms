5217e54329c7841f1a81d441829db46ee4afb3f2
Nick Fausti <ncfausti@gmail.com>
2016-01-11 02:59:51 -0500

    moved stack client code to main method of stack

--- Stack.java
+++ Stack.java
@@ -49,4 +49,19 @@ public class Stack<Item> implements Iterable<Item>
 			return item;
 		}
 	}
+
+	public static void main(String[] args)
+	{
+		Stack<String> stack = new Stack<String>();
+		
+		while(!StdIn.isEmpty())
+		{
+			String item = StdIn.readString();
+			if (!item.equals("-"))
+				stack.push(item);
+			else if (!stack.isEmpty()) StdOut.print(stack.pop() + " ");
+		}
+
+		StdOut.println("(" + stack.size() + " left on stack)");
+	}
 }
